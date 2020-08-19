ef2f09cc8044408c8bee8a919cf8378358c1e5c6
kevinelong <kevinelong@gmail.com>
2020-08-18 17:29:30 -0700

    updates

--- exception_handling.py
+++ exception_handling.py
@@ -1,14 +1,29 @@
+import sys
+
 try:
-    f = open("hope.txt", "r")
-    all = f.readlines()
-    for line in all:
-        print(line.strip('\n'))
-    f.close()
+    f = open("hope2.txt", "r")
+    all = list(f.readlines())
+
+except OSError as err:
+    print(f"OS error: {err}")
+    raise
+
+except ValueError:
+    print("Could not convert data to an integer.")
 
 except:
-    print("File not found or can’t be read.")
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
 
 finally:
     print("Cleaning Up")
+    if "f" in globals():
+        f.close()
 
-print("Finished")
+if "f" in globals():
+
+    for line in all:
+        print(line.strip('\n'))
+    f.close()
+    print("Finished")
+
