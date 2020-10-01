93e6f75d2623dd8ffbe8ef3ecf8143e732fe0cca
kevinelong <kevinelong@gmail.com>
2020-10-01 14:19:24 -0700

    updates

--- miko.py
+++ miko.py
@@ -1,32 +1,10 @@
-# netmikofrom netmiko import ConnectHandler
-#
-# linux = {
-#     'device_type': 'linux',
-#     'host':   '3.80.187.178',
-#     'username': 'kevin',
-#     'password': 'S!mpl312',
-# }
-# c = ConnectHandler(**linux) # use of kwargs optional, could just use regular parameters
-#
-# r1 = c.send_command("echo hello world  > hw.txt")
-# print(r1)
-#
-# r2 = c.send_command("cat hw.txt")
-# print(r2)
-#
-# r3 = c.send_command("ls -la")
-# print(r3)
-
-
 from netmiko import ConnectHandler
-import paramiko
-private_key_path = "~/.ssh/clvrclvr.pem"
+
 linux = {
     'device_type': 'linux',
-    'host':   'clvrclvr.pem',
+    'host':   '3.80.187.178',
     'username': 'kevin',
     'password': 'S!mpl312',
-    'pkey' : paramiko.RSAKey.from_private_key_file(private_key_path)
 }
 c = ConnectHandler(**linux) # use of kwargs optional, could just use regular parameters
 
@@ -38,3 +16,25 @@ print(r2)
 
 r3 = c.send_command("ls -la")
 print(r3)
+
+#
+# from netmiko import ConnectHandler
+# import paramiko
+# private_key_path = "~/.ssh/clvrclvr.pem"
+# linux = {
+#     'device_type': 'linux',
+#     'host':   'clvrclvr.com',
+#     'username': 'kevin',
+#     'password': 'S!mpl312',
+#     'pkey' : paramiko.RSAKey.from_private_key_file(private_key_path)
+# }
+# c = ConnectHandler(**linux) # use of kwargs optional, could just use regular parameters
+#
+# r1 = c.send_command("echo hello world  > hw.txt")
+# print(r1)
+#
+# r2 = c.send_command("cat hw.txt")
+# print(r2)
+#
+# r3 = c.send_command("ls -la")
+# print(r3)
