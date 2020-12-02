f80f79ede82ab9c20c7cc11dab362329ed481089
kevinelong <kevinelong@gmail.com>
2020-12-02 07:29:53 -0800

    sysos execute command and parse results update

--- sysos.py
+++ sysos.py
@@ -3,19 +3,29 @@ import subprocess
 
 def run(exe):
     cmd = exe.split(" ")
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
     return iter(p.stdout.readline, b'')
 
 
-result = run("ls -la")
+result = run("dir")
 # print(result)
 
-i = 0
+FILE_COLUMN = 4
+
+
+def is_valid(line_data):
+    return line_data and line_data[0] != "Directory" and line_data[-1] != "bytes"
+
+
 for line in result:
-    # print(line.decode().split(" ")) # WRONG
-    if i > 0:
-        print(i, line.decode().split(None)) #RIGHT deals with repeating white space
-    i += 1
+    raw_line = line.decode()
+    line_data = raw_line.split()
+
+    if is_valid(line_data):
+        file_name = line_data[FILE_COLUMN]
+        parts = file_name.split(".")
+        if parts[-1] == "txt":
+            print(file_name)
 
 # ---
 
