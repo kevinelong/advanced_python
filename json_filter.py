55ff7bf73eddf3d0e1cf518e8bb9f660fcb0137e
kevinelong <kevinelong@gmail.com>
2020-12-03 14:26:03 -0800

    list comprehension

--- json_filter.py
+++ json_filter.py
@@ -11,3 +11,8 @@ for e in data:
         output.append(e)
 
 print(output)
+
+# >>> even_squares = [x * x for x in range(10) if x % 2 == 0]
+# do it in one line with a list comprehension! ugly? hard to read? maybe.
+it_folks = [e for e in data if "IT" in e["title"]]
+print(it_folks)
